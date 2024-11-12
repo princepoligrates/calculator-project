1e657471199484575f52153ab2b71b3c54a8fb61
Prince Poligrates <princepoligrates@gmail.com>
2024-11-11 22:09:11 -0800

    initial commit

---
+++ src/OperationButton.js
@@ -0,0 +1,4 @@
+import { ACTIONS } from "./App"
+export default function OperationButtonButton({dispatch, operation}) {
+    return <button onClick={() => dispatch({type: ACTIONS.CHOOSE_OPERATION, payload: {operation} })}>{operation}</button>
+}
